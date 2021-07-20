510764829958a39b2e2169bcae444383b8663a1d
Amilcar <davidglez.ucol@gmail.com>
2021-07-20 11:11:21 -0500

    modificaciÃon del los estilos a CSS normal

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { CitasApp } from './CitasApp';
-import './styles/index.scss';
+import './styles/main.css';
 
 ReactDOM.render(
   <React.StrictMode>
